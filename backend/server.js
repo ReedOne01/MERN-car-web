e5702563917d7b5645b0a3549d8bb8a586059656
ladex lad <ladexlad2@gmail.com>
2023-07-12 03:53:34 +0100

    first commit

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`sever listening on ${port}`);
+});
